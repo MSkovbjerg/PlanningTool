7556c81b6b004eed8ab6d5c60d939d74b109c840
Skovbjerg <Skovbjerg@Skovbjerg-PC>
2016-04-11 21:03:27 +0200

    Test for opret projekt er startet

--- PlanningTool/tests/testOpretProjekt.java
+++ PlanningTool/tests/testOpretProjekt.java
@@ -1,4 +1,13 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class testOpretProjekt {
+	
+	/**
+	 * Test for oprettelse af projekt
+	 * 1. Medarbejderen skal logges ind.
+	 * 2. Et nyt projekt oprettes med navn og et projektnummer i form af et årstal og et løbenummer, feks. "030901".
+	 * 3. Der tjekkes om projektet er oprettet. 
+	 */
 
 }
